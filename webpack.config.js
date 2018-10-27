9146fef8a35e46a1228dce2ae5034ad9913c3083
your name here <nickbielak95@gmail.com>
2018-10-26 18:01:30 -0700

    Add bundle

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './frontend/todo.jsx',
     output: {
         path: path.resolve(__dirname),
-        filename: 'bundle.js'
+        filename: './bundle.js'
     },
     module: {
         rules: [
